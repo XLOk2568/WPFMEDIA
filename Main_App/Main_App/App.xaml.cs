502dd37cc484778507c06156186b981130df414b
XiaLiang <140861510+XLOk2568@users.noreply.github.com>
2025-01-28 19:45:34 +0800

    Add files via upload

---
+++ Main_App/Main_App/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using static NavigationViewExample.Pages.MainControlPage;
+
+namespace NavigationViewExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string Run_Time_Path = $"{Environment.CurrentDirectory}" + "\\AppD";
+        public static string setting_path = $"{Environment.CurrentDirectory}" + "\\AppD\\setting.txt";//其他窗口也用这个
+        public static string InterfaceLanguage_All = "";//其实是选择的语言文本的所有
+        public static string settings_ = "";
+        public static List<string> setting_all_list = App.settings_.Split("${@}").ToList();
+        public static List<string> IL_all_list = App.settings_.Split("${@}").ToList();
+        public static string Mediaworkfile = "";
+        public static string Temp_Lrc_txt ="";//放到LRC。xaml获取歌词的
+    }
+}
+    
+
+
--- Main_App/Main_App/MainWindow.xaml.cs
+++ Main_App/Main_App/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
-using iNKORE.UI.WPF.Modern;
+﻿using iNKORE.UI.WPF.Modern;
 using iNKORE.UI.WPF.Modern.Controls;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,9 +22,9 @@ namespace NavigationViewExample
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    { 
-        public string setting_path = $"{Environment.CurrentDirectory}" + "\\AppD\\setting.txt";//其他窗口也用这个
-
+    {
+        //setting.txt
+        //749,1266,258,123${@}dark${@}CN${@}firsttime,newtime${@}playtime,palyalist
         public int setting_homelist_count = 0;
         public MainWindow()
         {
@@ -32,55 +33,71 @@ namespace NavigationViewExample
         //这里说下方案，在主页弄两个frame，一个MainNv是用来作为用户主要的交互导航，另一个MainMini则是用来作为用户的进度条等等播放控制
         public Pages.MainNvPage PageNv_Load= new Pages.MainNvPage();
         public Pages.MainControlPage PageControl_Load = new Pages.MainControlPage();
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //判断是否存在
-            if (!System.IO.File.Exists(setting_path))
+            App.settings_ = File.ReadAllText(App.setting_path);
+            List<string> setting_homelist = App.settings_.Split("${@}").ToList();
+            setting_homelist_count = setting_homelist.Count;
+            //读取和设置屏幕 
+            List<string> tall2 = setting_homelist[0].Split(",").ToList();                                                            
+            Double happ = Double.Parse(tall2[0]);
+            Height = happ;
+            Double wapp = Double.Parse(tall2[1]);
+            Width = wapp;
+            Double lapp = Double.Parse(tall2[2]);
+            Left = lapp;
+            Double tapp = Double.Parse(tall2[3]);
+            Top = tapp;
+            //设置主题          
+            List<string> theme = App.settings_.Split("${@}").ToList();                                                                        
+            if (setting_homelist[1] == "dark")
             {
-                StreamWriter sm1 = new StreamWriter(setting_path);
-                sm1.Write(Height + "," + Width + "," + Left + "," + Top+"${@}dark");
-                sm1.Close();
+                ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
             }
-            else if (System.IO.File.Exists(setting_path))
+            else if (setting_homelist[1] == "light")
             {
-                List<string> setting_homelist = File.ReadAllText(setting_path).Split("${@}").ToList();
-                setting_homelist_count = setting_homelist.Count;
-                List<string> tall2 = setting_homelist[0].Split(",").ToList();//读取和设置屏幕
-                Double happ = Double.Parse(tall2[0]);
-                Height = happ;
-                Double wapp = Double.Parse(tall2[1]);
-                Width = wapp;
-                Double lapp = Double.Parse(tall2[2]);
-                Left = lapp;
-                Double tapp = Double.Parse(tall2[3]);
-                Top = tapp;
-                List<string> theme = File.ReadAllText(setting_path).Split("${@}").ToList();//设置主题
-                if (setting_homelist[1] == "dark")
-                {
-                    ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
-                }
-                else if (setting_homelist[1] == "light")
-                {
-                    ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
-                }
-                else if (setting_homelist[1] == "null")
-                {
-                    ThemeManager.Current.ApplicationTheme = null;
-                }
+                   ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+            }
+            //设置语言
+            if (setting_homelist[2] == "CN") //中
+            {
+                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\CN.txt");
+                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
+            }
+            else if (setting_homelist[2] == "EN")//英文
+            {
+                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\EN.txt");
+                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
+            }
+            if (setting_homelist[2] == "JP")//日语
+            {
+                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\JP.txt");
+                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
+            }
+            if (setting_homelist[2] == "FR")//法语
+            {
+                App.InterfaceLanguage_All = File.ReadAllText(App.Run_Time_Path + "\\Language_S\\FR.txt");
+                App.IL_all_list = App.InterfaceLanguage_All.Split("${@}").ToList();
             }
             Frame_Main_MainNv.Navigate(PageNv_Load);
             Frame_Main_MainControl.Navigate(PageControl_Load);
         }
+        private async void Load_IL()
+        {
+            await Task.Run(() =>
+            {
+
+            });
+        }
         private void _Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             // 弹窗提示是否确定要退出
-            MessageBoxResult f1 = iNKORE.UI.WPF.Modern.Controls.MessageBox.Show("你确定退出?", "Double check", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            MessageBoxResult f1 = iNKORE.UI.WPF.Modern.Controls.MessageBox.Show(App.IL_all_list[0], App.IL_all_list[0], MessageBoxButton.OKCancel, MessageBoxImage.Question);
             if (f1 == MessageBoxResult.OK)
             {
                 //文本文件中的第一行内容获取，并且被设置为app的屏幕值
                 //保存软件的屏幕位置和大小后退出
-                List<string> setting_homelist = File.ReadAllText(setting_path).Split("${@}").ToList();
+                List<string> setting_homelist = File.ReadAllText(App.setting_path).Split("${@}").ToList();
                 string less = setting_homelist[0];
                 string iftxt = Height + "," + Width + "," + Left + "," + Top;
                 if (less == iftxt)
@@ -89,15 +106,15 @@ namespace NavigationViewExample
                 }
                 else if (less!= iftxt)
                 {
-                    StreamWriter sm1 = new StreamWriter(setting_path);
-                    int Count = setting_homelist_count - 2;
-                    for (int T0 = 1; T0 !=Count; T0 += 1)
+                    int TrueCount = setting_homelist.Count;
+                    string Data = iftxt;
+                    for (int for_ = 1; for_ != TrueCount; for_ += 1)
                     {
-                        iftxt = iftxt + "${@}" + setting_homelist[T0];
+                        Data = Data + "${@}" + setting_homelist[for_];
                     }
-                    iftxt = iftxt + "${@}" + setting_homelist[setting_homelist_count-1];
-                    sm1.Write(iftxt);
-                    sm1.Close();                        
+                    StreamWriter sm1 = new StreamWriter(App.setting_path);
+                    sm1.Write(Data);
+                    sm1.Close();
                     Process.GetCurrentProcess().Kill();
                 }
             }
